9e3a93a863b2edb8f035e6a7b6cb3d12c593c3ad
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-12-21 17:18:30 -0300

    tests(mock): add mock to requests

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -5,10 +5,14 @@ __date__ = "$Dez 21, 2022 11:03:00 AM$"
 
 
 from time import time
+from time import mktime
+from datetime import datetime
+from datetime import timedelta
 from re import match
 from json import loads
 from unittest import TestCase
 from unittest.mock import patch
+from unittest.mock import Mock
 from app import app as web_api
 
 
@@ -60,6 +64,38 @@ class TestWebApiApp(TestCase):
     @patch("app.redis")
     def test_convert_notupdate_usd_to_brl(self, redis_mock):
         redis_mock.get.return_value = str(int(time()))
+        response = self.app.post(
+            '/convert',
+            query_string={
+                'from': 'USD',
+                'to': 'BRL',
+                'amount': 100.50
+            }
+        )
+        assert 200 == response.status_code
+        assert 'data' in loads(response.data)
+        assert 'message' in loads(response.data)
+        assert match(r'^Successfully exchanged', loads(response.data)['message'])
+
+    @patch("app.redis")
+    @patch("controllers.exchange.requests")
+    def test_convert_update_usd_to_brl(self, redis_mock, request_mock):
+        redis_mock.get.return_value = str(int(mktime((datetime.now() - timedelta(hours=2)).timetuple())))
+        redis_mock.set.return_value = 'OK'
+        response_mock = Mock(status_code=200)
+        response_mock.json.return_value = {
+            'success': True,
+            'timestamp': 1671651783,
+            'base': 'USD',
+            'date': '2022-12-21',
+            'rates': {
+                'USD': 1,
+                'BRL': 5.199896,
+                'EUR': 0.94212,
+                'BTC': 5.9627876e-05
+            }
+        }
+        redis_mock.get.return_value = response_mock
         response = self.app.post(
             '/convert',
             query_string={
